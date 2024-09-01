e4843e0bfbf09cca0e5c202e951de32f0a60bc5e
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-09-01 16:08:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
     //Write your code here
-  
+	let rInput = document.getElementById('radius').value;
+	const radius = parseFloat(rInput);
+
+	if(isNaN(radius) || radius < 0) {
+		document.getElementById('volume').value = 'NaN';
+	}else {
+		 const volume = (4/3) * Math.PI * Math.pow(radius, 3);
+		const roundedVol = volume.toFixed(4);
+		document.getElementById('volume').value = roundedVol;
+	}
+	
+  return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
